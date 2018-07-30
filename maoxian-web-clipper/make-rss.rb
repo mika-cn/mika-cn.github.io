a60d6b2806d063279384ea000beb04baf06c77a4
mika <mika@nothing.org>
2018-07-31 00:55:01 +0800

    [release] maoxian web clipper 0.1.3

--- maoxian-web-clipper/make-rss.rb
+++ maoxian-web-clipper/make-rss.rb
@@ -9,6 +9,12 @@ rss = RSS::Maker.make("atom") do |maker|
 
   news = [
     {
+      version: '0.1.3',
+      content: "MaoXian Web Clipper 0.1.3 is released",
+      link: "https://mika-cn.github.io/maoxian-web-clipper/change-log.html?#v013",
+      created_at: "2018-07-31 01:00:00 +0800"
+    },
+    {
       version: '0.1.2',
       content: "MaoXian Web Clipper 0.1.2 is released, [important] bug on windows platform fixed.",
       link: "https://mika-cn.github.io/maoxian-web-clipper/change-log.html?#v012",
